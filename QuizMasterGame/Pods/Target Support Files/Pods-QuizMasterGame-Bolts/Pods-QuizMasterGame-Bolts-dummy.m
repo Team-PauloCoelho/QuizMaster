2b63b2092c002fbc885fb52d72ae6cb70eacc11f
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2014-11-05 07:44:34 -0800

    Add initial project

---
+++ QuizMasterGame/Pods/Target Support Files/Pods-QuizMasterGame-Bolts/Pods-QuizMasterGame-Bolts-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QuizMasterGame_Bolts : NSObject
+@end
+@implementation PodsDummy_Pods_QuizMasterGame_Bolts
+@end
---
+++ QuizMasterGame/QuizMasterGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuizMasterGame
+//
+//  Created by Jack Leon on 11/4/14.
+//  Copyright (c) 2014 PauloCoelho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
